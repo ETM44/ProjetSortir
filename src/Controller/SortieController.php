7abb9de606bef87c6c2902488f87542cd30fd8c0
lguigand2021 <lola.guigand2021@campus-eni.fr>
2021-10-13 17:29:06 +0200

    Annuler et modifier sortie ok, reste à gérer miniature google Maps

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Sortie;
+use App\Entity\Site;
+use App\Entity\Lieu;
+use Symfony\Component\WebLink\Link;
 
 class SortieController extends AbstractController
 {
@@ -30,23 +41,87 @@ class SortieController extends AbstractController
     }
 
     /**
-     * @Route("sortie/annuler", name="app_annulerSortie")
+     * @Route("sortie/annuler/{id}", name="app_annulerSortie")
      * @param $id
      * @param SortieRepository $repository
      * @return Response
      */
     
-    public function annulerSortie($id,SortieRepository $repository): Response{
+    public function annulerSortie($id,SortieRepository $repository,Request $request, EntityManagerInterface $em): Response{
         $sortie=$repository->find($id);
-       /* $em->remove($sortie);
-        $em->flush();
 
-        return $this->redirectToRoute("main");
-       */
+
+        $newSortie = new Sortie();
+        $form = $this->createFormBuilder($newSortie)
+            ->add('infosSortie', TextareaType::class)
+            ->add('Confirmer',SubmitType::class)
+            ->add('Annuler',ResetType::class)
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $sortie->setInfosSortie($newSortie->getInfosSortie());
+            $sortie->setEtatSortie(6);
+            $em->flush();
+            $this->addFlash('success', 'Votre sortie a bien été annulée.');
+            return $this->redirectToRoute("main");
+        }
+
         return $this->render("sortie/annulerSortie.html.twig", [
             "title" => "Annuler une sortie",
-            "sortie"=>$this.sortie,
+            "sortie"=>$sortie,
+            "form"=>$form->createView()
             ]);
-        
     }
+
+    /**
+     * @Route("sortie/modifier/{id}", name="app_modifierSortie")
+     * @param $id
+     * @param SortieRepository $repository
+     * @return Response
+     */
+    public function modifierSortie($id,SortieRepository $repository,Request $request, EntityManagerInterface $em)
+    {
+        $sortie = $repository->find($id);
+
+        $updateSortieForm = $this->createFormBuilder($sortie)
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('site',EntityType::class,[
+                'class' => Site::class,
+                'choice_label' => function ($site) {
+                    return $site->getNom();
+                }
+            ])
+            ->add('lieu', EntityType::class, [
+                'class'=> Lieu::class,
+                'choice_label'=>function($lieu){
+                return $lieu->getNom();
+                }
+
+            ])
+            ->add('Enregistrer', SubmitType::class)
+            ->add('PublierLaSortie', SubmitType::class)
+            ->add('AnnulerLesModifications', ResetType::class)
+            ->getForm();
+        $updateSortieForm->handleRequest($request);
+
+        if ($updateSortieForm->isSubmitted() && $updateSortieForm->isValid()) {
+            $em->persist($sortie);
+            $em->flush();
+            $this->addFlash('success', 'Votre sortie a bien été modifiée.');
+            return $this->redirectToRoute("sortie/detail/{id}");
+        }
+
+        return $this->render("sortie/modifierSortie.html.twig", [
+            "title" => "Modifier une sortie",
+            "sortie" => $sortie,
+            "updateSortieForm" => $updateSortieForm->createView()
+            ]);
+    }
+    
 }
