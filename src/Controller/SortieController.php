68e07f015eb2de9a187786b0f359561e4acf5a75
lguigand2021 <lola.guigand2021@campus-eni.fr>
2021-10-13 11:37:42 +0200

    debut annuler sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Sortie;
 
 class SortieController extends AbstractController
 {
@@ -24,5 +27,26 @@ class SortieController extends AbstractController
     public function creerSortie(): Response
     {
 
+    }
+
+    /**
+     * @Route("sortie/annuler", name="app_annulerSortie")
+     * @param $id
+     * @param SortieRepository $repository
+     * @return Response
+     */
+    
+    public function annulerSortie($id,SortieRepository $repository): Response{
+        $sortie=$repository->find($id);
+       /* $em->remove($sortie);
+        $em->flush();
+
+        return $this->redirectToRoute("main");
+       */
+        return $this->render("sortie/annulerSortie.html.twig", [
+            "title" => "Annuler une sortie",
+            "sortie"=>$this.sortie,
+            ]);
+        
     }
 }
